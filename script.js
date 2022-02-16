94d62699d52bb1aae7791315f69d5d4357df741d
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-02-16 17:40:06 -0300

    req 9 completo

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const list = document.querySelector('#lista-tarefas');
 button.addEventListener('click', buttonToDoList);
 function buttonToDoList() {
   if (input.value !== '') {
-    console.log(input.value);
     let toDo = createListToDo();
     toDo.innerText = input.value;
     input.value = '';
@@ -15,7 +14,6 @@ function buttonToDoList() {
 
 function createListToDo() {
   let li = document.createElement('li');
-  li.classList.add('list-to-do');
   list.appendChild(li);
   return li;
 }
@@ -24,7 +22,6 @@ list.addEventListener('click', selectedItemColor);
 function selectedItemColor(event) {
   removeId();
   event.target.id = 'selected-item';
-  console.log(event.target);
 }
 
 function removeId() {
@@ -32,3 +29,17 @@ function removeId() {
     document.querySelector('#selected-item').removeAttribute('id');
   }
 }
+
+list.addEventListener('dblclick', lineThrough)
+function lineThrough(event) {
+  // let lineTest = throughTest(event.target)
+  if (event.target.className === 'completed') {
+    event.target.classList.remove('completed');
+  } else { event.target.classList.add('completed'); }
+}
+
+// function throughTest(test) {
+//   if (test.target.style.textDecoration === 'line-through') {
+//     test.classList.remove('completed');
+//   } else { test.classList.add('completed'); }
+// }
